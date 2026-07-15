b0507f398158cd19362227c5c8b7a06653706108
0xzxzoo <213499333+0xzxzoo@users.noreply.github.com>
2026-07-15 13:38:58 +0000

    condense 0.1.5

--- Formula/condense.rb
+++ Formula/condense.rb
@@ -3,8 +3,8 @@
 class Condense < Formula
   desc "CLI qui optimise les tokens envoyés à Claude & Codex"
   homepage "https://github.com/PENRA-CORP/Condense"
-  url "https://registry.npmjs.org/@penra/condense/-/condense-0.1.4.tgz"
-  sha256 "e43acdd225dea436411b2d4790ceb355bf23c5bac6ff699ea99044257829d194"
+  url "https://registry.npmjs.org/@penra/condense/-/condense-0.1.5.tgz"
+  sha256 "25346bbc7f14b7cd74c77433a851cde70ac715c074b6c2ca81dc5c8e9ba5155b"
   license "MIT"
   depends_on "node"
 
